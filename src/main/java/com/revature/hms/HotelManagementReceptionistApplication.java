ccc05a43fce1b8ad587638f469b1674fc0a46a99
Na-veed <naveedimran2877@gmail.com>
2021-09-12 18:15:34 +0530

    first commit

---
+++ src/main/java/com/revature/hms/HotelManagementReceptionistApplication.java
@@ -0,0 +1,13 @@
+package com.revature.hms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HotelManagementReceptionistApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HotelManagementReceptionistApplication.class, args);
+	}
+
+}
